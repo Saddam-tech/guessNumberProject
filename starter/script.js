2bfee41aeeae38932153081e250afb5742c36f10
Saddam-tech <salokhiddinov0727@gmail.com>
2021-01-25 09:42:12 +0900

    added comments

--- starter/script.js
+++ starter/script.js
@@ -9,10 +9,17 @@ document.querySelector('.number').textContent = secretNumber;
 const x = function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  // When guess does not exist
+
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+
+    // When guess is correct
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
@@ -22,6 +29,8 @@ const x = function () {
       document.querySelector('.message').textContent = 'You lost the game (';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
